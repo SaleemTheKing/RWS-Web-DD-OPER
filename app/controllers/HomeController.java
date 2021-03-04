9a7ac87a37ebb3e583c2478069f5305cc22dafde
SaleemTheKing <saleem031998@gmail.com>
2021-03-04 19:34:30 +0100

    Add database and register function

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,7 +1,15 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import models.Gebruiker;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
+import saleem.orm.utils.HibernateUtil;
+
+import javax.persistence.EntityManager;
 
 public class HomeController extends Controller {
 
@@ -10,6 +18,7 @@ public class HomeController extends Controller {
     }
 
     public Result start() {
+
         return ok(views.html.start.render());
     }
 
@@ -17,4 +26,37 @@ public class HomeController extends Controller {
         return ok(views.html.test1.render());
     }
 
+
+    public Result Register(Http.Request request) {
+
+        JsonNode node = request.body().asJson();
+        String email =  node.get("email").textValue();
+        String password = node.get("password").textValue();
+//        // FIXME: We are not checking if the email already exists.
+        SessionFactory sessionFactory = HibernateUtil.getSession();
+        Session session = sessionFactory.getCurrentSession();
+        EntityManager em = sessionFactory.createEntityManager();
+        em.getTransaction().begin();
+        Gebruiker gebruiker = new Gebruiker(email, password);
+        session.beginTransaction();
+        session.save(gebruiker);
+        session.getTransaction().commit();
+        session.close();
+        return ok(views.html.login.render());
+    }
+
+
+    public Result tryLogin(String email, String password) {
+        SessionFactory session = HibernateUtil.getSession();
+        EntityManager em = session.createEntityManager();
+        em.getTransaction().begin();
+
+        Gebruiker user = (Gebruiker) em.createQuery("from Gebruiker g where g.email = :email and g.password = :password")
+                .setParameter("email", email)
+                .setParameter("password", password)
+                .getSingleResult();
+        if (user != null) return ok(views.html.start.render());
+        else return badRequest("Error wrong credentials");
+
+    }
 }
